c9af8a0ad6f7a49931925de431dcc52dbc914802
Dudukin <edudukin2@gmail.com>
2020-02-03 22:00:18 +0200

    Working with form validation and handling

--- js/main.js
+++ js/main.js
@@ -54,33 +54,69 @@ function loginClick() {
 
 function contactClick() {
     var errors = false;
-    if (document.getElementById('form-name').value === '') {
-        $('#form-name-label').text('Please enter your full name');
+    let input = {
+        name: document.getElementById('form-name'),
+        company: document.getElementById('form-comp'),
+        email: document.getElementById('form-email'),
+        phone: document.getElementById('form-phone'),
+        country: document.getElementById('form-country'),
+    };
+    let regexp = {
+        email: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
+        phone: /^\d+$/
+    };
+    if (input.name.value === '') {
+        $('#form-name-label').text('Пожалуйста, введите полное имя');
+        $('#form-name').css('border-bottom-color', '#red');
+        errors = true;
+    } else {
+        $('#form-name-label').text('Подходит!');
         $('#form-name').css('border-bottom-color', '#3FD3AD');
-        errors = true;
     }
-    if (document.getElementById('form-comp').value === '') {
-        $('#form-comp-label').text('Please enter your company');
+    if (input.company.value === '') {
+        $('#form-comp-label').text('Пожалуйста, введите компанию');
+        $('#form-comp').css('border-bottom-color', '#red');
+        errors = true;
+    } else {
+        $('#form-comp-label').text('Подходит!');
         $('#form-comp').css('border-bottom-color', '#3FD3AD');
-        errors = true;
     }
-    if (document.getElementById('form-email').value === '') {
-        $('#form-email-label').text('Please enter your email');
-        $('#form-email').css('border-bottom-color', '#FF4F1A');
+
+    if (input.email.value === '') {
+        $('#form-email-label').text('Пожалуйста, введите email');
+        $('#form-email').css('border-bottom-color', '#red');
         errors = true;
+    } else if (!regexp.email.test(input.email.value)) {
+        $('#form-email-label').text('Пожалкйста, введите корректный email');
+        $('#form-email').css('border-bottom-color', '#red');
+        errors = true;
+    } else {
+        $('#form-email-label').text('Подходит!');
+        $('#form-email').css('border-bottom-color', '#3FD3AD');
     }
-    if (document.getElementById('form-phone').value === '') {
-        $('#form-phone-label').text('Please enter your phone');
-        $('#form-phone').css('border-bottom-color', '#FF4F1A');
+
+    if (input.phone.value === '') {
+        $('#form-phone-label').text('Пожалуйста, введите телефон');
+        $('#form-phone').css('border-bottom-color', '#red');
         errors = true;
+    } else if (!regexp.phone.test(input.phone.value)) {
+        $('#form-phone-label').text('Пожалуйста, введите корректный телефон (только цифры)');
+        $('#form-phone').css('border-bottom-color', '#red');
+        errors = true;
+    } else {
+        $('#form-phone-label').text('Подходит!');
+        $('#form-phone').css('border-bottom-color', '#3FD3AD');
     }
-    if (document.getElementById('form-country').value === '') {
-        $('#form-country-label').text('Please enter your country');
-        $('#form-country').css('border-bottom-color', '#FF4F1A');
+    if (input.country.value === '') {
+        $('#form-country-label').text('Пожалуйста, введите страну');
+        $('#form-country').css('border-bottom-color', '#red');
         errors = true;
+    } else {
+        $('#form-country-label').text('Подходит!');
+        $('#form-country').css('border-bottom-color', '#3FD3AD');
     }
     if (errors) {
-        $('#form-btn-contact').css('border-color', '#3FD3AD');
+        $('#form-btn-contact').css('border-color', '#red');
     }
     if (!errors) {
         $('#contactModal .wrapper').addClass('animated fadeOut');
@@ -91,7 +127,7 @@ function contactClick() {
         $('#form-btn-contact').css('display', 'none');
         $(target).removeClass('animated');
         $(target).removeClass('fadeInUp');
-        setTimeout(function () {
+        setTimeout(function() {
             $('#contactModal .wrapper').css('display', 'none');
             $('#contactModal .modal-login-heading').css('display', 'none');
             $(target).css('visibility', 'visible');
@@ -99,12 +135,56 @@ function contactClick() {
 
             $.ajax({
                 type: "POST",
-                url: "/mail.php",
-                data: $('form').serialize()
+                url: "php/handlers/ContactFormHandler.php",
+                data: $('#contact_form').serialize(),
             });
         }, 1500);
     }
 };
+
+
+function subscribeForm() {
+
+    let submitButton = document.querySelectorAll('.footer-subscribe-form-btn')[0];
+    let subscribeForm = document.querySelectorAll('form.footer-subscribe-form')[0];
+    let subscribeInput = subscribeForm.childNodes[1];
+    let regexp = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    let errors = false;
+    let thankNode = document.createElement("p");
+    thankNode.classList.add('footer-subscribe-thankyou');
+    submitButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (subscribeInput.value === '') {
+            thankNode.innerHTML = 'Пожалуйста, введите ваш email';
+            thankNode.classList.add('footer-subscribe-wrong');
+            subscribeForm.append(thankNode);
+            errors = true;
+        } else if (!regexp.test(subscribeInput.value)) {
+            thankNode.innerHTML = 'Пожалуйста, введите корректный email';
+            thankNode.classList.add('footer-subscribe-wrong');
+            subscribeForm.append(thankNode);
+            errors = true;
+        } else {
+            thankNode.classList.remove('footer-subscribe-wrong');
+            thankNode.innerHTML = '';
+            errors = false;
+        }
+
+        if (!errors) {
+            $.ajax({
+                type: "POST",
+                url: "php/handlers/SubscribeHandler.php",
+                data: $('form.footer-subscribe-form').serialize(),
+                success: function () {
+                    thankNode.innerHTML = 'Спасибо за подписку!';
+                    subscribeForm.append(thankNode);
+                    subscribeInput.value = '';
+                }
+            });
+        }
+
+    });
+}
 
 function contactClickRu() {
     var errors = false;
@@ -246,4 +326,5 @@ $(document).ready(function () {
     slideDownArrow();
     anchorNav();
     revealText();
+    subscribeForm();
 });
